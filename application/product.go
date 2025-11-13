fd271ffa9bec9828f6b7c08b29875e4d58c22f3a
Gilson Luchesi Correa <correa_gilson@hotmail.com>
2025-11-13 13:35:29 -0300

    implementação service

--- application/product.go
+++ application/product.go
@@ -99,7 +99,7 @@ func (p *Product) Disable() error {
 	return errors.New("the price must be zero in order to have the product disabled")
 }
 
-func (p *Product) GetID() string {
+func (p *Product) GetId() string {
 	return p.ID
 }
 
